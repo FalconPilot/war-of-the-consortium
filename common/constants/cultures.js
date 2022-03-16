d3da5ca9af8a3f4b4d2b5ddb18368637db733b57
FalconPilot <fpilot.github@gmail.com>
2022-03-17 00:50:07 +0100

    Fix template, remove build artifacts

--- common/constants/cultures.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.cultures = void 0;
-exports.cultures = {
-    kaelian: 'Kaelian',
-    hongxing: 'Hongxing',
-    huraas: 'Huraas',
-    roho: 'Roho'
-};
--- common/constants/locale.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.locales = void 0;
-exports.locales = {
-    fr: 'Français',
-    en: 'English'
-};
